b55c905258775cac84c6cd099cddb5807f643daf
Jordan Ball <cjordanball@ballwebdev.com>
2022-08-04 07:01:29 -0400

    add tabs to page

---
+++ assets/text/introduction.js
@@ -0,0 +1,5 @@
+export default {
+	headline1: 'A Brief Introduction',
+	article1: `An Array of Strings is a collective of string players who came together while working at the main office of WillowTree, a leading digital product consultancy located in the historic Woolen Mills section of Charlottesville/Albermarle County.\n\n
+		Currently we are messing around!`,
+};
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import Text from '../assets/text/introduction.js';
+
+// const articleElement = document.createElement('article');
+// articleElement.innerHTML = Text.article1;
+
+// console.log(articleElement);
+
+// const articleSection = document.getElementsByClassName('articleSection');
+
+// articleSection[0].appendChild(articleElement);
